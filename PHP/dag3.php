60a54156fa11811ec704b65729520cb97175ba92
SebastianSlangerup <Sebastian.Slangerup1@gmail.com>
2020-08-21 18:36:46 +0200

    Mere arbejde på dag 3

--- PHP/dag3.php
+++ PHP/dag3.php
@@ -18,6 +18,32 @@
         $i++;
     }
     echo $sum;
+    echo "<hr>";
+
+        // Opgave 2
+    $maks = 10;
+    $i = 1;
+    $sum = 0;
+
+    // Do-loops vil altid kører mindst én gang
+    do {
+        echo $i, "<br>";
+        $sum += $i;
+        $i++;
+    } while ($i < $maks);
+    echo $sum;
+    echo "<hr>";
+
+        // Opgave 3
+    $sum = 0;
+    for ($i = 1; $i < $maks; $i++) { 
+        echo $i, "<br>";
+        $sum += $i;
+    }
+    echo $sum
+
+        // Opgave 4
+    // $i er 
     ?>
 </body>
 </html>
